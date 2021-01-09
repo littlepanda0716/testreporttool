6db37f24a96374bd992a215ce746e0837ffd3c53
littlepanda0716 <littlepanda0716@gmail.com>
2021-01-09 21:39:47 +0800

    Update report.py

--- report.py
+++ report.py
@@ -6,7 +6,7 @@ Created on Sat Jan  9 20:34:32 2021
 @author: imclumsypanda
 """
 import streamlit as st
-
+from PIL import Image
 
 st.header(':computer:  测试报告工具')
 nl=[5,8,3]
@@ -19,5 +19,9 @@ for j in range (3):
                 for ki in range(k):
                     st.text_input('填写issue '+str(j+1)+'.'+str(i+1)+'.'+str(ki+1)+' 名称',key=(i+1)*1000+ki*100+1)
                     st.text_area('填写issue '+str(j+1)+'.'+str(i+1)+'.'+str(ki+1)+' 描述',key=(i+1)*1000+ki*100+1)
-                    st.file_uploader('上传issue '+str(j+1)+'.'+str(i+1)+'.'+str(ki+1)+' 对应图片',type=None,accept_multiple_files=True,key=(i+1)*1000+ki*100+1)
+                    pics=st.file_uploader('上传issue '+str(j+1)+'.'+str(i+1)+'.'+str(ki+1)+' 对应图片',type=None,accept_multiple_files=True,key=(i+1)*1000+ki*100+1)
+                    if len(pics)>0:
+                        for picindex,pic in enumerate(pics):
+                            image1 = Image.open(pic)
+                            st.image(image1,caption='issue '+str(j+1)+'.'+str(i+1)+'.'+str(ki+1)+' 配图 '+str(picindex+1))
             
